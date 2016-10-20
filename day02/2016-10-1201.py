0f1ea79fb15320757a84fb536f788ca98ba44190
HSLoveZL <hjmrezl@outlook.com>
2016-10-20 23:57:53 +0800

    Add the readme file

--- day02/2016-10-1201.py
+++ day02/2016-10-1201.py
@@ -8,9 +8,11 @@ class Bird:
             self.hungry = False
         else:
             print 'No thanks!'
+
+
 class SongBird(Bird):
     def __init__(self):
-        #Bird.__init__(self)
+        # Bird.__init__(self)
         super(SongBird, self).__init__()
         self.sound = 'Hello!'
 
